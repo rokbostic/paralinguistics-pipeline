b5ca6a292186dc66ec80391ff5fe9b0b90e9daff
rokbostic <rb0171@student.uni-lj.si>
2026-06-22 01:06:08 +0200

    instruction fix 2

--- create_instructions.py
+++ create_instructions.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
-def kaldi_to_dict(filepath):
+def kaldi_to_dict(filepath, is_float=True):
     return {
-        k: float(v)
+        k: float(v) if is_float else v
         for k, v in (
             line.split(maxsplit=1)
             for line in filepath.read_text(encoding="utf-8").splitlines()
@@ -23,8 +23,8 @@ def main():
     full_count = 0
 
     emotions_list = [
-        kaldi_to_dict(f)
-        for f in sorted(Path("benchmark_outputs").glob("*emotions*"))
+        kaldi_to_dict(f, False)
+        for f in sorted(Path("outputs").glob("*emotions*"))
     ]
 
     speaking_rates = kaldi_to_dict(Path("outputs/utt2speakingrate"))
@@ -33,16 +33,21 @@ def main():
     arousal_rates = kaldi_to_dict(Path("utt2arousal"))
     arousal_avg = mean(arousal_rates.values())
 
-    with open("instructions", "w", encoding="utf-8") as f:
+    with open("outputs/instructions", "w", encoding="utf-8") as f:
         for utt in emotions_list[0]:
             emotions = {emotion[utt] for emotion in emotions_list}
             decided_emotion = next(iter(emotions)) if len(emotions) == 1 else None
 
-            speed = "hitro" if speaking_rates[utt] > speaking_rate_avg else "počasi"
-            arousal = "vzburjeno" if arousal_rates[utt] > arousal_avg else "nevzburjeno"
+
+
+            speed = speaking_rates.get(utt, speaking_rate_avg)
+            arousal = arousal_rates.get(utt, arousal_avg)
+
+            speed_str = "hitro" if speed > speaking_rate_avg else "počasi"
+            arousal_str = "vzburjeno" if arousal > arousal_avg else "nevzburjeno"
 
             emotion_text = f" in v čustvu {decided_emotion}" if decided_emotion else ""
-            instruction = f"Govori {speed} in {arousal}{emotion_text}."
+            instruction = f"Govori {speed_str} in {arousal_str}{emotion_text}."
 
             f.write(f"{utt} {instruction}\n")
 
